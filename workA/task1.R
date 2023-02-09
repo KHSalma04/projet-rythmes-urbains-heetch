475a0bc7b73554c1675abfcd205c3b183704be1c
KHMASSI Salma <120320847+KHSalma04@users.noreply.github.com>
2023-02-09 11:55:31 +0000

    Update task1.R

--- workA/task1.R
+++ workA/task1.R
@@ -1,28 +1,61 @@
-################################## #
-### Projet d'analyse VTC Heetch ## #
-################################## #
+############### Project #################
 
-# load packages ----
-library(dplyr)
-library(sf) #données géométriques
-library(leaflet) #visualisation
-library(lubridate) #work with times
-library(mapsf) #as leaflet but static
-library(tidyr) #gerer les convestion de table: long to matric_carre
+# 1. Load packages ------
 
-# load data  ----
-casaBound <- st_read("data/casabound.geojson") #des poinrts qui forment une
-# un object spatial "simple features"
-osmFeatures <- readRDS("data/osmfeatures.Rds") 
-# load hetchpoints (a simple feature)
-heetch_points <- readRDS("data/heetchmarchcrop.Rds") #too much data
-# load neigbors
-casa_neib <- st_read("data/casaneib.geojson")
-head(casa_neib)
+library(dplyr) 
+library(sf)
+library(leaflet)
+library(lubridate)
+library(mapsf)
+library(tidyr)
 
-# project all data ----
-proj_crs <- 26191 #code de la proj nord maroc
-casaBound_proj = st_transform(casaBound, crs=proj_crs)
-heetch_points_proj = st_transform(heetch_points, crs=proj_crs)
-casa_neib_proj = st_transform(heetch_points, crs=proj_crs)
-rm(proj_crs)
+# 2. Load data -------
+
+heetchPoints <-readRDS("DATA/heetchmarchcrop.Rds")
+casaNeib <-st_read("DATA/casaneib.geojson") 
+
+# 3. Projeter les objets spatiaux ------
+
+casaNeibProj <-st_transform(casaNeib, crs=26191)
+heetchPointsProj <-st_transform(heetchPoints, crs=26191)
+
+# 4. Créer la section du 1er Mars ------
+
+heetchPointsProj$Day <-day(heetchPointsProj$location_at_local_time)
+heetchPointsProj$Hour <-hour(heetchPointsProj$location_at_local_time)
+heetchM1Proj <-heetchPointsProj %>% 
+  filter(Day==1) %>% 
+  filter(Hour>22 | Hour<7 )
+
+# 5. Intersecter les points x et les quartiers y ------
+
+casaNeibUnion <-st_union(casaNeibProj)
+ptsInCasa <-st_contains(x=casaNeibUnion, y=heetchM1Proj)
+
+selectPtsInCasa <-unlist(ptsInCasa)
+
+heetchM1CropProj <-heetchM1Proj[selectPtsInCasa,]
+ptsInNeib <-st_within(x=heetchM1Proj, y=casaNeibProj) %>% 
+  unlist() 
+heetchM1CropProj$NEIB <-ptsInNeib
+
+# 6. Grouper par heure/ par identifiant chauffeur et par quartier --------
+
+nestedPts <-heetchM1CropProj %>% 
+  st_drop_geometry() %>% 
+  group_by(Hour, NEIB, driver_id) %>% 
+  summarize(NBPTS=n())
+
+# 7. Selectionner le quartier principal pour chaque chauffeur à chaque heure
+
+mainNeib <-nestedPts %>% 
+  group_by(driver_id, Hour) %>% 
+  arrange(desc(NBPTS)) %>% 
+  slice(1)
+
+# 8. Pour chaque heure, pour chaque quartier, calculer le nombre de chauffeurs
+
+mainNeib <-nestedPts %>% 
+  group_by(Hour, NEIB) %>% 
+  summarize(NBPDR=n())
+
